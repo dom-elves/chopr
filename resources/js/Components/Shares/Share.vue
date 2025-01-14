5e49bc2322f29afc83851f60339e0caed26565a5
Dom Elves <dom_elves@hotmail.co.uk>
2025-01-14 23:22:58 +0000

    borders to show cleared/not cleared shares

--- resources/js/Components/Shares/Share.vue
+++ resources/js/Components/Shares/Share.vue
@@ -12,7 +12,10 @@ onMounted(() => console.log(props.share));
 </script>
 
 <template>
-    <div class="p-1 m-2 border-solid border-2 border-emerald-600">
+    <div 
+        class="p-1 m-2 border-solid border-2" 
+        :class="share.cleared ? 'border-emerald-600' : 'border-red-600'"
+    >
         <p>{{ share.group_user.user.name }}</p>
         <p>{{ share.amount }}</p>
     </div>
