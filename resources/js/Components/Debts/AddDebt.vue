3ad1e7c07184ab942cbe8a35b32d86a0764449b4
Dom Elves <dom_elves@hotmail.co.uk>
2025-01-16 15:10:39 +0000

    split debt even, custom debt amount

--- resources/js/Components/Debts/AddDebt.vue
+++ resources/js/Components/Debts/AddDebt.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed, onMounted, onUnmounted, ref, reactive } from 'vue';
+import { computed, onMounted, onUnmounted, ref, reactive, watch } from 'vue';
 
 
 const props = defineProps({
@@ -11,19 +11,34 @@ const props = defineProps({
 const showAddDebtForm = ref(false);
 
 // generate form data object based on groupUsers passed in as a prop
+// todo: see if there's a way to improve this
 const addDebtForm = reactive({
     debtName: '',
+    debtAmount: 0,
     selectedUsers: props.groupUsers.reduce((accumulator, user) => {
-        console.log('this', accumulator, user);
-        accumulator[user.id] = false;
+        accumulator[user.id] = 0;
         return accumulator;
-    }, {})
+    }, {}),
+    splitEven: false,
 });
 
 function addDebt() {
 //   router.post('/debts/save', form)
 console.log('form', addDebtForm);
 }
+
+// splits the debt evenly on checkbox
+// resets to 0 on unchecked
+watch(() => addDebtForm.splitEven, () => {
+    const amount = addDebtForm.debtAmount / props.groupUsers.length;
+    for (const user in addDebtForm.selectedUsers) {
+        if (addDebtForm.splitEven) {
+            addDebtForm.selectedUsers[user] = amount;
+        } else {
+            addDebtForm.selectedUsers[user] = 0;
+        }
+    }
+});
 </script>
 
 <template>
@@ -31,20 +46,43 @@ console.log('form', addDebtForm);
         <button class="bg-blue-400 text-white p-2" @click="showAddDebtForm = !showAddDebtForm">Add a debt</button>
         <div v-show="showAddDebtForm">
             <form @submit.prevent="addDebt">
-                <label for="debt-name">Debt Name</label>
-                <input
-                    v-model="addDebtForm.debtName" 
-                    type="text" 
-                    id="debt-name" 
-                    name="debt-name" 
-                    class="p-2 m-2 border-solid border-2 border-gray-400"
-                />
+                <div>
+                    <label for="debt-name">Debt Name</label>
+                    <input
+                        v-model="addDebtForm.debtName" 
+                        type="text" 
+                        id="debt-name" 
+                        name="debt-name" 
+                        class="p-2 m-2 border-solid border-2 border-gray-400"
+                    />
+                </div>
+                <div>
+                    <label for="debt-amount">Amount:</label>
+                    <input
+                        v-model="addDebtForm.debtAmount"
+                        type="number"
+                        id="debt-amount"
+                        name="debt-amount"
+                    />
+                </div>
+                <div>
+                    <label for="split-even">Split even?</label>
+                    <input
+                        v-model="addDebtForm.splitEven" 
+                        type="checkbox" 
+                        name="split-even" 
+                        id="split-even" />
+                </div>
                 <div v-for="group_user in props.groupUsers">
                     <label :for="group_user.id">{{ group_user.user.name }}</label>
                     <input
                         v-model="addDebtForm.selectedUsers[group_user.id]" 
-                        type="checkbox" 
+                        type="number"
+                        step="0.01" 
                         :id="group_user.id"
+                        :disabled="addDebtForm.splitEven"
+                        class="disabled:bg-slate-50"
+                        @change="splitEven"
                     />
                 </div>
                 <button class="bg-blue-400 text-white p-2" type="submit">Save</button>
