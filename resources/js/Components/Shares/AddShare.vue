9b6e2c5d3c8c53ab2afdcd3a875544eb94bca6d0
Dom Elves <dom_elves@hotmail.co.uk>
2025-01-20 12:12:29 +0000

    basic AddShare component

---
+++ resources/js/Components/Shares/AddShare.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { data } from 'autoprefixer';
+import { computed, onMounted, onUnmounted, ref } from 'vue';
+
+const props = defineProps({
+    groupUser: {
+        type: Object,
+    },
+});
+
+const selected = ref(false);
+
+// onMounted(() => console.log('share', props.groupUser));
+
+</script>
+
+<template>
+    <div
+        @click="selected = !selected"
+        class="border-solid border-2 border-green-600 bg-white m-2 p-2 flex flex-row"
+        :class="selected ? 'bg-green-200' : ''"
+    >
+        <label 
+            :for="groupUser.id">{{ groupUser.user.name }}
+        </label>
+        <input
+            :name="`groupUser-${groupUser.id}`"
+            @click.stop
+            v-show="selected"
+            type="number"
+            step="0.01" 
+            :id="groupUser.id"
+            class="disabled:bg-slate-50" 
+        />
+    </div>
+</template>
