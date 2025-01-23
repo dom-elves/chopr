5195f26caf28f89f0fb0c99ba6ecc68ac2933fe6
Dom Elves <dom_elves@hotmail.co.uk>
2025-01-23 14:26:36 +0000

    very basic implementation of delete debt modal, not styled

--- resources/js/Components/Debts/Debt.vue
+++ resources/js/Components/Debts/Debt.vue
@@ -2,17 +2,29 @@
 import { computed, onMounted, onUnmounted, ref } from 'vue';
 import { router } from '@inertiajs/vue3';
 import Share from '@/Components/Shares/Share.vue';
+import Modal from '@/Components/Modal.vue';
 
 const props = defineProps({
     debt: {
         type: Object,
     },
 });
+const confirmingDebtDeletion = ref(false);
+
+const confirmDebtDeletion = () => {
+    confirmingDebtDeletion.value = true;
+
+    nextTick(() => passwordInput.value.focus());
+};
 
 function deleteDebt() {
     console.log('delete', props.debt.id);
     router.delete(route('debt.destroy', { debt_id: props.debt.id }));
 }
+
+const closeModal = () => {
+    confirmingDebtDeletion.value = false;
+};
 
 </script>
 
@@ -20,7 +32,7 @@ function deleteDebt() {
     <div class="m-2 border-solid border-2 border-amber-600">
         <div class="flex flex-row justify-between">
             <p> {{ debt.name }} {{ debt.amount }}</p>
-            <div class="border-solid border-2 border-red-600" @click="deleteDebt">X</div>
+            <div class="border-solid border-2 border-red-600" @click="confirmDebtDeletion">X</div>
         </div>
         <div class="flex flex-row flex-wrap justify-evenly">
             <Share
@@ -29,5 +41,27 @@ function deleteDebt() {
             >
             </Share>
         </div>
+        <Modal :show="confirmingDebtDeletion" @close="closeModal">
+            <div class="p-6">
+                <h2
+                    class="text-lg font-medium text-gray-900"
+                >
+                    Are you sure you want to delete this debt?
+                </h2>
+                <div class="mt-6 flex justify-end">
+                    <button 
+                        @click="closeModal"
+                    >
+                        Cancel
+                    </button>
+                    <button
+                        class="ms-3"
+                        @click="deleteDebt"
+                    >
+                        Delete Debt
+                    </button>
+                </div>
+            </div>
+        </Modal>
     </div>
 </template>
