f98eb2efeca086ad08d9c5153230357918d3d87a
Dom Elves <dom_elves@hotmail.co.uk>
2025-01-31 20:20:29 +0000

    basic group user component

---
+++ resources/js/Components/GroupUsers/GroupUser.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+import { computed, onMounted, onUnmounted, ref, reactive } from 'vue';
+const props = defineProps({
+    group_user: {
+        type: Object,
+    },
+});
+
+onMounted(() => console.log(props.group_user));
+
+
+</script>
+
+<template>
+    <div>
+        {{ group_user.user.name }}
+    </div>
+</template>
--- resources/js/Components/Groups/ViewGroups.vue
+++ resources/js/Components/Groups/ViewGroups.vue
@@ -1,7 +1,7 @@
 <script setup>
 
 import { computed, onMounted, onUnmounted, ref, reactive } from 'vue';
-
+import GroupUser from '@/Components/GroupUsers/GroupUser.vue';
 const props = defineProps({
     groups: {
         type: Object,
@@ -15,6 +15,24 @@ onMounted(() => console.log(props.groups));
 
 <template>
     <div>
-        groups
+        <!-- 
+            add group component (with email invite)
+            loop over groups to view
+                group user component
+                maybe into user component?
+        -->
+                <div 
+                    v-for="group in groups"
+                    class="p-4 m-2 border-solid border-2 border-indigo-600"
+                >
+                    <h3 class="text-3xl text-center mb-4">
+                        {{ group.name }}
+                    </h3>
+                    <GroupUser
+                        v-for="group_user in group.group_users"
+                        :group_user="group_user"
+                        >
+                    </GroupUser>
+                </div>
     </div>
 </template>
