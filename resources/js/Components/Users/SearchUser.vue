1fe0665028ff9b2130dcd7a735d8708675398a4c
Dom Elves <dom_elves@hotmail.co.uk>
2025-02-04 15:55:12 +0000

    small amount of styling on search results

--- resources/js/Components/Users/SearchUser.vue
+++ resources/js/Components/Users/SearchUser.vue
@@ -33,18 +33,43 @@ onMounted(() => {
 <template>
     <div>
         <button class="bg-blue-400 text-white p-2 w-full" @click="showAddUserButton = !showAddUserButton" v-if="showAddUserButton">Add a user</button>
-        <div class="my-2 border-solid border-2 border-amber-600 flex justify-center" v-else>
-            <form @submit.prevent>
-                <label for="add-group-user">Search:</label>
-                <input type="text" id="add-group-user" v-model="query_string" @change="search">
-            </form>
-            <i 
-                class="fa-solid fa-x mx-1"
-                @click="showAddUserButton = !showAddUserButton"
-            >
-            </i>
+        <div class="my-2 border-solid border-2 border-amber-600 flex flex-col justify-center items-center" v-else>
+            <div class="flex flex-row">
+                <form @submit.prevent>
+                    <label 
+                        for="add-group-user" 
+                        class="hidden" 
+                        id="user-search"
+                    >
+                        Search:
+                    </label>
+                    <input 
+                        type="text" 
+                        id="add-group-user" 
+                        aria-labelledby="user-search"
+                        v-model="query_string" 
+                        @change="search"
+                        placeholder="Search for a user..."
+                    >
+                </form>
+                <i 
+                    class="fa-solid fa-x mx-1"
+                    @click="showAddUserButton = !showAddUserButton"
+                >
+                </i>
+            </div>
             <div v-if="results.user_results != null">
-                <p v-for="user in results.user_results">{{ user.name }}</p>
+                <div 
+                    v-for="user in results.user_results" 
+                    style="background-color:#ffffff" 
+                    class="p-2 flex flex-row items-center justify-between"
+                >
+                    <div class="flex flex-col">
+                        <p>{{ user.name }}</p>
+                        <small class="text-gray-300">{{ user.email }}</small>
+                    </div>
+                    <i class="fa-solid fa-plus"></i>
+                </div>
             </div>
         </div>
     </div>
