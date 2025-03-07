1b3eb40ff0e4e21f606ea422236ebb8e52e1d28c
Dom Elves <dom_elves@hotmail.co.uk>
2025-03-07 15:29:16 +0000

    mostly fixed fe but weird bug with 0/1/false/true

--- resources/js/Components/Shares/Share.vue
+++ resources/js/Components/Shares/Share.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed, onMounted, ref, watch } from 'vue';
+import { computed, onMounted, ref, watch, } from 'vue';
 import { currencies } from '@/currencies.js';
 import { router, useForm, usePage } from '@inertiajs/vue3';
 
@@ -20,18 +20,18 @@ const isDebtOwner = props.debtOwnerId === props.share.group_user_id;
 // check if logged in user is share owner
 const isShareOwner = props.share.group_user.user.id === usePage().props.auth.user.id;
 
-let isShareSent = ref(props.share.sent);
-let isShareSeen = ref(props.share.seen);
+let isShareSent = ref(props.share.sent ? true : false);
+let isShareSeen = ref(props.share.seen ? true : false);
 
-const updateShareForm = useForm({
+const sendShareForm = useForm({
     id: props.share.id,
     sent: isShareSent.value,
-    seen: isShareSeen.value,
     group_user_id: props.share.group_user_id,
 });
 
-function updateShare() {
-    updateShareForm.patch(route('share.update'), {
+function sendShare() {
+    console.log('form', sendShareForm);
+    sendShareForm.patch(route('share.update'), {
         preserveScroll: true,
         onSuccess: () => {
             console.log('sent');
@@ -39,9 +39,24 @@ function updateShare() {
     });
 }
 
+const seenShareForm = useForm({
+    id: props.share.id,
+    seen: isShareSeen.value,
+    group_user_id: props.share.group_user_id,
+});
+
+function seenShare() {
+    console.log('form', seenShareForm);
+    seenShareForm.patch(route('share.update'), {
+        preserveScroll: true,
+        onSuccess: () => {
+            console.log('seen');
+        },
+    });
+}
+
 onMounted(() => {
-    // console.log('id on load', props.share.id, isShareOwner);
-    console.log(isShareOwner, props.share.id);
+    console.log(`share ${props.share.id} sent: ${isShareSent.value}`);
 });
 
 // todo: figure out a way to stop having to use this function in multiple places
@@ -62,7 +77,7 @@ const debtCurrency = computed(() => {
             style="height:70px"
         >
             <p>
-                {{ share.group_user.user.name }}
+                {{ share.group_user.user.name }} {{ share.id }}
                 <small v-if="isDebtOwner">
                     owner
                 </small>
@@ -80,7 +95,7 @@ const debtCurrency = computed(() => {
                     <label 
                         style="height:40px;width:40px;border-radius:50%" 
                         class="border-solid border-2 flex justify-center items-center"
-                        :class="updateShareForm.sent ? 'border-green-400' : 'border-red-400'"
+                        :class="sendShareForm.sent ? 'border-green-400' : 'border-red-400'"
                         :for="'sent-' + share.id"
                     >
                         <i class="fa-solid fa-check"></i>
@@ -88,9 +103,9 @@ const debtCurrency = computed(() => {
                     <input 
                         type="checkbox" 
                         :id="'sent-' + share.id" 
-                        class="hidden" 
-                        @click="updateShare"
-                        v-model="updateShareForm.sent"
+                        class="hidden"
+                        @click="sendShare"
+                        v-model="sendShareForm.sent"
                     >
                 </form>
             </div>
@@ -100,7 +115,7 @@ const debtCurrency = computed(() => {
                     <label 
                         style="height:40px;width:40px;border-radius:50%" 
                         class="border-solid border-2 flex justify-center items-center"
-                        :class="updateShareForm.seen ? 'border-green-400' : 'border-red-400'"
+                        :class="seenShareForm.seen ? 'border-green-400' : 'border-red-400'"
                         :for="'seen-' + share.id"
                     >
                         <i class="fa-solid fa-check"></i>
@@ -109,8 +124,8 @@ const debtCurrency = computed(() => {
                         type="checkbox" 
                         :id="'seen-' + share.id" 
                         class="hidden" 
-                        @click="updateShare"
-                        v-model="updateShareForm.seen"
+                        @click="seenShare"
+                        v-model="seenShareForm.seen"
                     >
                 </form>
             </div>
