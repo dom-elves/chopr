286722ad635fb9e86d92af67b64a3073ac2fae67
Dom Elves <dom_elves@hotmail.co.uk>
2025-03-20 11:55:38 +0000

    adjustments to middleware

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -34,9 +34,7 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         $user = $request->user();
-
-        $group_user_ids = $user ? GroupUser::where('user_id', $user->id)->pluck('id')->toArray() : [];
-
+  
         return array_merge(parent::share($request), [
             'auth' => [
                 'user' => $user,
@@ -44,8 +42,8 @@ class HandleInertiaRequests extends Middleware
             'ownership' => [
                 // groups that the logged in user owns
                 'group_ids' => $user ? Group::where('user_id', $user->id)->pluck('id')->toArray() : [],
-                // debt ids owned by the logged in user
-                'debt_ids' => !empty($group_user_ids) ? Debt::whereIn('user_id', $group_user_ids)->pluck('id')->toArray() : [],
+                // debts & shares owned by the logged in user
+                'debts' => $user ? Debt::where('user_id', $user->id)->with('shares')->get() : [],
                 'comment_ids' => $user ? Comment::where('user_id', $user->id)->pluck('id')->toArray() : [],
             ]
         ]);
