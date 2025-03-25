b9dfbdb0f3a830dbfcf69b6e5e2311ffefbac696
Dom Elves <dom_elves@hotmail.co.uk>
2025-03-25 14:12:17 +0000

    update naming convention for share tests

--- tests/Feature/Http/Controllers/ShareControllerTest.php
+++ tests/Feature/Http/Controllers/ShareControllerTest.php
@@ -18,7 +18,7 @@ beforeEach(function () {
     $this->group = $this->group_user->group;
 
     // since these are shares tests, debts will always be involved
-    $this->debt = Debt::where('collector_group_user_id', $this->group_user->id)->first();
+    $this->debt = Debt::where('user_id', $this->user->id)->first();
     $this->shares = $this->debt->shares;
 
     $this->actingAs($this->user);
@@ -26,7 +26,7 @@ beforeEach(function () {
 
 test("user can select 'sent' on their own share", function () {
     // get a share i own
-    $share = $this->shares->where('group_user_id', $this->group_user->id)->first();
+    $share = $this->shares->where('user_id', $this->user->id)->first();
 
     // update it
     $response = $this->patch(route('share.update'), [
@@ -46,7 +46,7 @@ test("user can select 'sent' on their own share", function () {
 test("user can not select 'sent' on a share they do not own", function () {
     // get a share that's not mine
     $share = $this->shares->reject(fn($share) => 
-        $share->group_user_id === $this->group_user->id)->first();
+        $share->user_id === $this->user->id)->first();
 
     // try to update it
     $response = $this->patch(route('share.update'), [
@@ -67,7 +67,7 @@ test("user can not select 'sent' on a share they do not own", function () {
 test("user can select 'seen' on the share for a debt they own", function () {
     // get a share that's not mine
     $share = $this->shares->reject(fn($share) => 
-        $share->group_user_id === $this->group_user->id)->first();
+        $share->user_id === $this->user->id)->first();
     
     // try to update it
     $response = $this->patch(route('share.update'), [
@@ -89,10 +89,10 @@ test("user can not select 'seen' on the share for a debt they do not own", funct
     // a share i don't own, in a debt i don't own
     $debts = Debt::where('group_id', $this->group->id)->get();
     $debt = $debts->reject(fn($debt) => 
-        $debt->collector_group_user_id === $this->group_user->id)->first();
+        $debt->user_id === $this->user->id)->first();
 
     $share = $debt->shares->reject(fn($share) => 
-        $share->group_user_id === $this->group_user->id)->first();
+        $share->user_id === $this->user->id)->first();
 
    // try to update it
     $response = $this->patch(route('share.update'), [
