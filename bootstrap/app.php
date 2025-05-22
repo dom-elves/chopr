2468d860880f9e989aaf575e3a47bf3356ffe98e
Dom Elves <dom_elves@hotmail.co.uk>
2025-05-22 10:51:43 +0100

    trust proxies to prevent cors issues

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         //
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
