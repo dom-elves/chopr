5ff0a848e61a95de1c9efbb095708527bcd1c2e8
Dom Elves <dom_elves@hotmail.co.uk>
2025-05-29 15:44:44 +0100

    forgot namespace

--- database/seeders/BasicLoginSeeder.php
+++ database/seeders/BasicLoginSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\Group;
 
 class BasicLoginSeeder extends Seeder
 {
