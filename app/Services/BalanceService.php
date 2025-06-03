d6d78ac1a2bf3f88f3125085d4d4c06198d17f46
Dom Elves <dom_elves@hotmail.co.uk>
2025-06-03 15:00:27 +0100

    refactor to be DRY

--- app/Services/BalanceService.php
+++ app/Services/BalanceService.php
@@ -9,15 +9,12 @@ class BalanceService
 {
     public function addToGroupUserBalance($share): void
     {
-        $share_owner = $share->user;
-        $debt_owner = $share->debt->user;
-        $share_group_user = $share_owner->group_users->where('user_id', $share->user_id)->first();
-        $debt_group_user = $debt_owner->group_users->where('user_id', $debt_owner->id)->first();
-        
+        [$share_group_user, $debt_group_user] = $this->getGroupUsers($share);
+
         $share_group_user->balance += $share->amount;
         $share_group_user->save();
 
-        if ($share_owner != $debt_owner) {
+        if ($share_group_user != $debt_group_user) {
             $debt_group_user->balance -= $share->amount;
             $debt_group_user->save();
         } else {
@@ -27,15 +24,12 @@ class BalanceService
 
     public function updateGroupUserBalance($share, $difference): void
     {
-        $share_owner = $share->user;
-        $debt_owner = $share->debt->user;
-        $share_group_user = $share_owner->group_users->where('user_id', $share->user_id)->first();
-        $debt_group_user = $debt_owner->group_users->where('user_id', $debt_owner->id)->first();
+        [$share_group_user, $debt_group_user] = $this->getGroupUsers($share);
         
         $share_group_user->balance += $difference;
         $share_group_user->save();
 
-        if ($share_owner != $debt_owner) {
+        if ($share_group_user != $debt_group_user) {
             $debt_group_user->balance -= $difference;
             $debt_group_user->save();
         } else {
@@ -45,20 +39,27 @@ class BalanceService
 
     public function subtractFromGroupUserBalance($share): void
     {
-        $share_owner = $share->user;
-        $debt_owner = $share->debt->user;
-        $share_group_user = $share_owner->group_users->where('user_id', $share->user_id)->first();
-        $debt_group_user = $debt_owner->group_users->where('user_id', $debt_owner->id)->first();
-        
+        [$share_group_user, $debt_group_user] = $this->getGroupUsers($share);
+
         $share_group_user->balance -= $share->amount;
         $share_group_user->save();
 
-        if ($share_owner != $debt_owner) {
+        if ($share_group_user != $debt_group_user) {
             $debt_group_user->balance += $share->amount;
             $debt_group_user->save();
         } else {
             return;
         }
+    }
+
+    private function getGroupUsers(Share $share)
+    {
+        $share_owner = $share->user;
+        $debt_owner = $share->debt->user;
+        $share_group_user = $share_owner->group_users->where('user_id', $share->user_id)->first();
+        $debt_group_user = $debt_owner->group_users->where('user_id', $debt_owner->id)->first();
+
+        return [$share_group_user, $debt_group_user];
     }
 }
  
