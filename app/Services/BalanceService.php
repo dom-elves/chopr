e514436bca81fdf84d109354404013ac001943b3
Dom Elves <dom_elves@hotmail.co.uk>
2025-06-03 14:50:53 +0100

    expanded logic for updated & delete share methods

--- app/Services/BalanceService.php
+++ app/Services/BalanceService.php
@@ -27,18 +27,38 @@ class BalanceService
 
     public function updateGroupUserBalance($share, $difference): void
     {
-        $user = $share->user;
-        $group_user = $user->group_users->where('user_id', $share->user_id)->first();
-        $group_user->balance += $difference;
-        $group_user->save();
+        $share_owner = $share->user;
+        $debt_owner = $share->debt->user;
+        $share_group_user = $share_owner->group_users->where('user_id', $share->user_id)->first();
+        $debt_group_user = $debt_owner->group_users->where('user_id', $debt_owner->id)->first();
+        
+        $share_group_user->balance += $difference;
+        $share_group_user->save();
+
+        if ($share_owner != $debt_owner) {
+            $debt_group_user->balance -= $difference;
+            $debt_group_user->save();
+        } else {
+            return;
+        }
     }
 
     public function subtractFromGroupUserBalance($share): void
     {
-        $user = $share->user;
-        $group_user = $user->group_users->where('user_id', $share->user_id)->first();
-        $group_user->balance -= $share->amount;
-        $group_user->save();
+        $share_owner = $share->user;
+        $debt_owner = $share->debt->user;
+        $share_group_user = $share_owner->group_users->where('user_id', $share->user_id)->first();
+        $debt_group_user = $debt_owner->group_users->where('user_id', $debt_owner->id)->first();
+        
+        $share_group_user->balance -= $share->amount;
+        $share_group_user->save();
+
+        if ($share_owner != $debt_owner) {
+            $debt_group_user->balance += $share->amount;
+            $debt_group_user->save();
+        } else {
+            return;
+        }
     }
 }
  
