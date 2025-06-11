b15577107c2e61a71ec7e51cc68c91f002e1093d
Dom Elves <dom_elves@hotmail.co.uk>
2025-06-11 14:10:42 +0100

    tests for add/delete split even debts

--- tests/Feature/TotalBalanceTest.php
+++ tests/Feature/TotalBalanceTest.php
@@ -66,13 +66,12 @@ test("adding a standard debt recalculates the user balances", function() {
         } else {
             $calced_balance = strval(round($original_balance - $share_amount, 2));
             $this->assertSame($new_balance, $calced_balance);
-         
         }
     }
 });
 
 test("deleting a standard debt recalculates the user's balance", function() {
-    $debt = Debt::first();
+    $debt = Debt::where('split_even', 0)->first();
 
     $user_shares = [];
 
@@ -102,7 +101,6 @@ test("deleting a standard debt recalculates the user's balance", function() {
         } else {
             $calced_balance = strval(round($original_balance + $share_amount, 2));
             $this->assertSame($new_balance, $calced_balance);
-         
         }
     }
 });
@@ -112,12 +110,83 @@ test("updating a standard debt recalculates the user's balance", function() {
     // depends on what i decide to do with discrepancies 
 });
 
+/**
+ * identical to add debt test for standard debts
+ */
 test("adding a split even debt recalculates the user's balance", function() {
+    $debt_total = 100;
+    $group = Group::where('user_id', $this->self->id)->first();
 
+    $user_shares = selectRandomGroupUsers($group->users, $debt_total, false);
+
+    foreach ($user_shares as &$share_data) {
+        $user = User::findOrFail($share_data['user_id']);
+        $share_data['original_balance'] = $user->user_balance;
+    }
+    unset($share_data);
+ 
+    // post debt
+    $response = $this->post(route('debt.store'), [
+        'group_id' => $group->id,
+        'user_id' => $this->self->id,
+        'name' => 'test debt 456',
+        'amount' => $debt_total,
+        'split_even' => 1,
+        'user_shares' => $user_shares,
+        'currency' => 'GBP',
+    ]);
+
+    $debt = Debt::where('name', "test debt 456")->first();
+
+    foreach ($user_shares as $share_data) {
+        $user = User::findOrFail($share_data['user_id']);
+        $share_amount = $share_data['amount'];
+        $original_balance = $share_data['original_balance'];
+        $new_balance = strval(round($user->user_balance, 2));
+
+        if ($user->id === $debt->user_id) {
+            $difference = $debt->shares->sum('amount') - $share_amount;
+            $calced_balance = strval(round($original_balance + $difference, 2));
+            $this->assertSame($new_balance, $calced_balance);
+        } else {
+            $calced_balance = strval(round($original_balance - $share_amount, 2));
+            $this->assertSame($new_balance, $calced_balance);
+        }
+    }
 });
 
 test("deleting a split even debt recalculates the user's balance", function() {
+    $debt = Debt::where('split_even', 1)->first();
 
+    $user_shares = [];
+
+    foreach ($debt->shares as $share) {
+        $user_shares[] = [
+            'amount' => $share->amount,
+            'original_balance' => $share->user->user_balance,
+            'user_id' => $share->user_id
+        ];
+    }
+ 
+    $response = $this->delete(route('debt.destroy'), [
+        'id' => $debt->id
+    ]);
+
+    foreach ($user_shares as $share_data) {
+        $user = User::findOrFail($share_data['user_id']);
+        $share_amount = $share_data['amount'];
+        $original_balance = $share_data['original_balance'];
+        $new_balance = strval(round($user->user_balance, 2));
+        
+        if ($user->id === $debt->user_id) {
+            $difference = $debt->shares->sum('amount') - $share_amount;
+            $calced_balance = strval(round($original_balance - $difference, 2));
+            $this->assertSame($new_balance, $calced_balance);
+        } else {
+            $calced_balance = strval(round($original_balance + $share_amount, 2));
+            $this->assertSame($new_balance, $calced_balance);
+        }
+    }
 });
 
 test("updating a split even debt recalculates the user's balance", function() {
