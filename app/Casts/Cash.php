aba50ccfa2ee096754b14a20d2858a84ea794d6e
Dom Elves <dom_elves@hotmail.co.uk>
2025-06-30 12:16:20 +0100

    fix cash cast, initially misudnerstood it

--- app/Casts/Cash.php
+++ app/Casts/Cash.php
@@ -15,24 +15,12 @@ class Cash implements CastsAttributes
      */
     public function get(Model $model, string $key, mixed $value, array $attributes): mixed
     {
-        switch (class_basename($model)):
-            case 'Debt':
-            case 'Share':
-                $type = 'amount';
-                break;
-            case 'GroupUser':
-                $type = 'balance';
-                break;
-            default: 
-                throw new \InvalidArgumentException("Unsupported model type: " . class_basename($model));
-        endswitch;
-        
         // default everything to GBP for now
         // far in the future, work out some way to support multiple currencies
         // probably by calling an exchange rate API to addyour various balances together
         // and convert to GBP
         // todo: this^
-        return Money::ofMinor($attributes[$type], 'GBP');
+        return Money::ofMinor($value, 'GBP');
     }
 
     /**
@@ -46,6 +34,6 @@ class Cash implements CastsAttributes
             return $value;
         }
 
-        return $value->getMinorAmount->toInt();
+        return $value->getMinorAmount()->toInt();
     }
 }
