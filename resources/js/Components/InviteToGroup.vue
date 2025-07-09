f79b555e36da872b63f1a66ea2ec9c9b1d5a7675
Dom Elves <dom_elves@hotmail.co.uk>
2025-07-09 10:23:47 +0100

    basically empty  component with closeable modal

---
+++ resources/js/Components/InviteToGroup.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { computed, onMounted, onUnmounted, ref, reactive } from 'vue';
+import Modal from '@/Components/Modal.vue';
+
+const openModal = ref(false);
+
+</script>
+<template>
+    <div>
+        <button 
+            class="bg-blue-400 text-white p-2 w-full" 
+            @click="openModal = !openModal"
+            >
+                Invite
+        </button>
+        <Modal 
+            :show="openModal" 
+            :closeable="true" 
+            @close="openModal = !openModal"
+            >
+                <p>some stuff</p>
+        </Modal>
+    </div>
+</template>
