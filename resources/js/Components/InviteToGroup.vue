b33c345af85c6def395dce0ef1923491727e1665
Dom Elves <dom_elves@hotmail.co.uk>
2025-07-09 11:29:02 +0100

    basic building of address list

--- resources/js/Components/InviteToGroup.vue
+++ resources/js/Components/InviteToGroup.vue
@@ -1,24 +1,48 @@
 <script setup>
-import { computed, onMounted, onUnmounted, ref, reactive } from 'vue';
+import { ref } from 'vue';
+import { router, useForm, usePage } from '@inertiajs/vue3';
+import { recipients } from '@/invite.js';
 import Modal from '@/Components/Modal.vue';
 
 const openModal = ref(false);
+const emailInput = ref('');
+
+function addRecipient() {
+    recipients.emailAddresses.push(emailInput.value);
+}
+
+function removeRecipient(emailAddress) {
+    recipients.emailAddresses = recipients.emailAddresses.filter((email) => email !== emailAddress);
+}
 
 </script>
+
 <template>
     <div>
+        <input
+            type="email"
+            v-model="emailInput"
+            @keydown.enter.prevent="addRecipient"
+        >
+        <span v-for="emailAddress in recipients.emailAddresses">
+            {{ emailAddress }}
+            <i
+                class="fa-solid fa-x mx-1"
+                @click="removeRecipient(emailAddress)"
+            ></i>
+        </span>
         <button 
             class="bg-blue-400 text-white p-2 w-full" 
             @click="openModal = !openModal"
-            >
-                Invite
+        >
+            Invite
         </button>
         <Modal 
             :show="openModal" 
             :closeable="true" 
             @close="openModal = !openModal"
-            >
-                <p>some stuff</p>
+        >
+            <p>some stuff</p>
         </Modal>
     </div>
 </template>
---
+++ resources/js/debt.js
@@ -0,0 +1,64 @@
+import { reactive } from 'vue';
+import Dinero from 'dinero.js'
+
+export const store = reactive({
+    addDebtForm: {
+        user_id: null,
+        group_id: null,
+        name: '',
+        currency: '',
+        user_shares: [], 
+        split_even: false,
+        amount: 0,
+    },
+
+    /**
+     * Adds share amounts together on input change for non split even debts
+     */
+    calcTotalAmount() {
+        const amount = Dinero({
+            amount: this.addDebtForm.user_shares.map(share => share.amount)
+                .reduce((acc, value) => acc + value, 0),
+            currency: this.addDebtForm.currency,
+        })
+
+        this.addDebtForm.amount = amount.getAmount();
+
+        console.log('form after calc total', this.addDebtForm);
+    },
+
+    /**
+     * Split even only runs on input change if the form is set to split even
+     * On a non split even debt, only the model is changing so this method never fires
+     */
+    splitEven() {
+        // total users being added 
+        const selectedUsersLength = this.addDebtForm.user_shares.filter((share) => share.checked == true).length;
+        
+        const amount = Dinero({
+            amount: this.addDebtForm.amount * 100,
+            currency: this.addDebtForm.currency,
+        });
+
+        const splits = [];
+
+        // this is how Dinero.js wants the splits to be defined
+        // e.g. a 3 person split is [1, 1, 1]
+        for (let i = selectedUsersLength; i > 0; i--) {
+            splits.push(1);
+        }
+
+        // handling the allocation of splits
+        const shares = amount.allocate(splits).map(s => s.getAmount());
+
+        // set as share amount for the user if they are selected
+        // by default, give the first user the slightly larger share
+        this.addDebtForm.user_shares.forEach((share) => {
+            share.amount = 0;
+
+            if (share.checked) {
+                share.amount = shares.shift() / 100;
+            }
+        });
+    },
+})
