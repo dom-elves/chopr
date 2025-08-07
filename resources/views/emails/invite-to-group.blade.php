3dda6a090c3f4b86c3be90f0553c55073ce1dbc8
Dom Elves <dom_elves@hotmail.co.uk>
2025-08-07 14:21:43 +0100

    adjsut mail to show different routes depending on user existence

--- resources/views/emails/invite-to-group.blade.php
+++ resources/views/emails/invite-to-group.blade.php
@@ -1,8 +1,13 @@
 <div>
     <p>{{ $invite->user->name }} has invited you to join the group, <i>{{ $invite->group->name }}</i>, on chopr:</p>
     <p><i>{{ $invite->body }}</i></p>
-    <a href="{{ route('invite.accept', ['token' => $invite->token]) }}">
-       {{  $is_new_user ? 'Sign up' : 'Join group' }}
-    </a>
-
+    @if ($is_new_user)
+        <a href="{{ route('invite.signup', ['token' => $invite->token]) }}">
+            {{ 'Sign up' }}
+        </a>
+    @else
+        <a href="{{ route('invite.join', ['token' => $invite->token]) }}">
+            {{ 'Join' }}
+        </a>
+    @endif
 </div>
