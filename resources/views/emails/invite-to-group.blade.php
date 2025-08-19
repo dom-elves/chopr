916f5c92d226f1b327751e2168458a20fe2a187f
Dom Elves <dom_elves@hotmail.co.uk>
2025-08-19 14:00:25 +0100

    remove whitespace

--- resources/views/emails/invite-to-group.blade.php
+++ resources/views/emails/invite-to-group.blade.php
@@ -2,8 +2,7 @@
     <p>{{ $invite->user->name }} has invited you to join the group, <i>{{ $invite->group->name }}</i>, on chopr:</p>
     <p><i>{{ $invite->body }}</i></p>
 
-        <a href="{{ route('invite.accept', ['token' => $invite->token]) }}">
-            {{ $is_new_user ? 'Sign up' : 'Join' }}
-        </a>
-
+    <a href="{{ route('invite.accept', ['token' => $invite->token]) }}">
+        {{ $is_new_user ? 'Sign up' : 'Join' }}
+    </a>
 </div>
