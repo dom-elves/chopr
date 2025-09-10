75eb7ab4a1f7d5eff7afb26c9f0cc48328208c1e
Dom Elves <dom_elves@hotmail.co.uk>
2025-09-10 14:02:31 +0100

    update create group form to use inertiajs form component, remove redunant test

--- tests/Feature/Http/Controllers/GroupControllerTest.php
+++ tests/Feature/Http/Controllers/GroupControllerTest.php
@@ -132,25 +132,6 @@ test('deleting a group deletes the relevant debts and shares', function() {
     } 
 });
 
-// test('deleting a group deletes the relevant shares', function() {
-//     $group = Group::where('user_id', $this->user->id)->first();
-//     $shares = $group->debts->first()->shares;
-
-//     $response = $this->delete(route('group.destroy'), [
-//         'id' => $group->id,
-//         'name' => $group->name,
-//         'user_id' => $this->user->id,
-//     ]);
-
-//     foreach ($shares as $share) {
-//         $this->assertDatabaseHas('shares', [
-//             'id' => $share->id,
-//             'debt_id' => $group->debts->first()->id,
-//             'deleted_at' => Carbon::now()->format('Y-m-d H:i:s'),
-//         ]);
-//     } 
-// });
-
 test('user can not delete a group they do not own', function() {
     $not_group_owner = $this->users->last();
     $this->actingAs($not_group_owner);
