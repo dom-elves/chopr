3c59d40ffb838a49ade42b9bfd096d69ee419356
Dom Elves <dom_elves@hotmail.co.uk>
2025-09-10 11:11:56 +0100

    playground stuff

--- resources/js/Pages/InertiaPlayground.vue
+++ resources/js/Pages/InertiaPlayground.vue
@@ -1,6 +1,9 @@
 <script setup>
 
 import { Head } from '@inertiajs/vue3';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import SecondaryButton from '@/Components/SecondaryButton.vue';
+import { Form } from '@inertiajs/vue3';
 
 defineProps({
     inertiaVariable: {
@@ -11,7 +14,14 @@ defineProps({
 
 <template>
     <Head title="Dashboard" />
-</br>
-    I wonder if this works
-    {{  inertiaVariable }}
+    <p>I wonder if this works</p>
+    <p>{{  inertiaVariable }}</p>
+    <div class="flex flex-col p-2">
+        <PrimaryButton>Primary Button</PrimaryButton>
+        <SecondaryButton>Secondary Button</SecondaryButton>
+        <Form>
+            <label for="test">Test</label>
+            <input type="text" name="test">
+        </Form>
+    </div>
 </template>
