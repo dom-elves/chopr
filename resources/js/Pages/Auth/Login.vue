282c4a3721c01b2f4852787aef59c9c73e71fc5c
Dom Elves <dom_elves@hotmail.co.uk>
2025-10-21 15:03:51 +0100

    remove very old debug

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -85,7 +85,7 @@ const submit = () => {
                     :href="route('password.request')"
                     class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
-                    Forgot your password test2333?
+                    Forgot your password?
                 </Link>
 
                 <PrimaryButton
