2e3605378cdb3a1a168ad3a54707ccbeb66ca1ad
Dom Elves <dom_elves@hotmail.co.uk>
2025-11-05 15:01:17 +0000

    adjust width & remove pointless div

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -15,11 +15,11 @@ onMounted(() => {
 
         setTimeout(() => {
             document.getElementById('slot-content').classList.add('fade-in');
-        }, 1000);
+        }, 1500);
 
         setTimeout(() => {
             document.getElementById('socials').classList.add('fade-in');
-        }, 1500);
+        }, 2500);
     } else {
         document.getElementById('title-image').classList.remove('fade-start');
         document.getElementById('beta').classList.remove('fade-start');
@@ -32,26 +32,24 @@ onMounted(() => {
 <template>
     <div class="bg-slate-100 w-full flex flex-col items-center">
         <header><!-- put something here one day --></header>
-        <main class="flex flex-col md:w-1/2 bg-white  min-h-screen items-center">
-            <div style="max-width:390px" class="flex flex-col items-center">
-                <a href="/">
-                    <img src="/storage/chopr-logo-2.png" class="mt-20 fade-start" id="title-image"/>
-                </a>
-                <p id="beta" class="fade-start">beta</p>
-                <!-- slot is things like registartion, pw recovery -->
-                <div id="slot-content" class="fade-start w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
-                    <slot />
-                </div>
-                <footer id="socials" style="position:fixed;bottom:0px"class="flex flex-row w-full p-4 md:w-1/2 justify-center fade-start">
-                    <a href="https://www.linkedin.com/in/dom-elves-681774200/">
-                        <i class="fa-brands fa-linkedin" style="font-size:35px"></i>
-                    </a>
-                    <div style="width:75px"></div>
-                    <a href="https://github.com/dom-elves/chopr">
-                        <i class="fa-brands fa-square-github" style="font-size:35px"></i>
-                    </a>
-                </footer>
+        <main class="flex flex-col md:w-1/2 bg-white min-h-screen items-center">
+            <a href="/" style="max-width:576px">
+                <img src="/storage/chopr-logo-2.png" class="mt-20 fade-start" id="title-image"/>
+            </a>
+            <p id="beta" class="fade-start">beta</p>
+            <!-- slot is things like registartion, pw recovery -->
+            <div id="slot-content" class="fade-start w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
+                <slot />
             </div>
+            <footer id="socials" style="position:fixed;bottom:0px"class="flex flex-row w-full p-4 md:w-1/2 justify-center fade-start">
+                <a href="https://www.linkedin.com/in/dom-elves-681774200/">
+                    <i class="fa-brands fa-linkedin" style="font-size:35px"></i>
+                </a>
+                <div style="width:75px"></div>
+                <a href="https://github.com/dom-elves/chopr">
+                    <i class="fa-brands fa-square-github" style="font-size:35px"></i>
+                </a>
+            </footer>
         </main>
     </div>
 </template>
