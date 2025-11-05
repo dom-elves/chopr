ba9df9893e47b90f2a7916a6e29f854ec5eeadd4
Dom Elves <dom_elves@hotmail.co.uk>
2025-11-05 14:34:59 +0000

    adjust GuestLayout to work with login & other pages thematically

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,22 +1,68 @@
 <script setup>
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import { Link } from '@inertiajs/vue3';
+import { onMounted, getCurrentInstance } from 'vue';
+
+onMounted(() => {
+    if (getCurrentInstance().parent.type.__name === 'Welcome') {
+        setTimeout(() => {
+            document.getElementById('title-image').classList.add('fade-in');
+        }, 500);
+
+        setTimeout(() => {
+            document.getElementById('beta').classList.add('fade-in');
+        }, 500);
+
+        setTimeout(() => {
+            document.getElementById('slot-content').classList.add('fade-in');
+        }, 1000);
+
+        setTimeout(() => {
+            document.getElementById('socials').classList.add('fade-in');
+        }, 1500);
+    } else {
+        document.getElementById('title-image').classList.remove('fade-start');
+        document.getElementById('beta').classList.remove('fade-start');
+        document.getElementById('slot-content').classList.remove('fade-start');
+        document.getElementById('socials').classList.remove('fade-start');
+    }
+})
 </script>
 
 <template>
-    <div
-        class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0"
-    >
-        <div>
-            <Link href="/">
-                <ApplicationLogo class="h-20 w-20 fill-current text-gray-500" />
-            </Link>
-        </div>
-
-        <div
-            class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg"
-        >
-            <slot />
-        </div>
+    <div class="bg-slate-100 w-full flex flex-col items-center">
+        <header><!-- put something here one day --></header>
+        <main class="flex flex-col md:w-1/2 bg-white  min-h-screen items-center">
+            <div style="max-width:390px" class="flex flex-col items-center">
+                <a href="/">
+                    <img src="/storage/chopr-logo-2.png" class="mt-20 fade-start" id="title-image"/>
+                </a>
+                <p id="beta" class="fade-start">beta</p>
+                <!-- slot is things like registartion, pw recovery -->
+                <div id="slot-content" class="fade-start w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
+                    <slot />
+                </div>
+                <footer id="socials" style="position:fixed;bottom:0px"class="flex flex-row w-full p-4 md:w-1/2 justify-center fade-start">
+                    <a href="https://www.linkedin.com/in/dom-elves-681774200/">
+                        <i class="fa-brands fa-linkedin" style="font-size:35px"></i>
+                    </a>
+                    <div style="width:75px"></div>
+                    <a href="https://github.com/dom-elves/chopr">
+                        <i class="fa-brands fa-square-github" style="font-size:35px"></i>
+                    </a>
+                </footer>
+            </div>
+        </main>
     </div>
 </template>
+<style scoped>
+
+.fade-start {
+  opacity: 0;
+  transition: opacity 1s ease-in;
+}
+
+.fade-start.fade-in {
+  opacity: 1;
+}
+</style>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,9 +1,7 @@
 <script setup>
-import Checkbox from '@/Components/Checkbox.vue';
 import { Head, Link } from '@inertiajs/vue3';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import SecondaryButton from '@/Components/SecondaryButton.vue';
 import { onMounted } from 'vue';
+import GuestLayout from '@/Layouts/GuestLayout.vue';
 
 
 defineProps({
@@ -24,67 +22,32 @@ defineProps({
 });
 
 onMounted(() => {
-    setTimeout(() => {
-        document.getElementById('title-image').classList.add('fade-in');
-    }, 500);
-
-    setTimeout(() => {
-        document.getElementById('links').classList.add('fade-in');
-    }, 1000);
-
-    setTimeout(() => {
-        document.getElementById('socials').classList.add('fade-in');
-    }, 1500);
+    
 });
 
 </script>
 
 <template>
     <Head title="Welcome" />
-    <div class="bg-slate-100 w-full flex flex-col items-center">
-        <header></header>
-        <div class="flex flex-col md:w-1/2 bg-white  min-h-screen items-center">
-            <div style="max-width:390px" class="flex flex-col items-center">
-                <img src="/storage/chopr-logo-2.png" class="mt-20 fade-start" id="title-image"/>
-                <main id="links" class="flex flex-row w-full mt-10 justify-center fade-start">
-                    <Link
-                        :href="route('login')"
-                        class="text-xl font-bold text-right uppercase rounded-md text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                        style="width:110px;"
-                    >
-                        Log in
-                    </Link>
-                    <div style="width:75px"></div>
-                    <Link
-                        v-if="canRegister"
-                        :href="route('register')"
-                        class="text-xl font-bold uppercase rounded-md text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                        style="width:110px;"
-                    >
-                        Register
-                    </Link>
-                </main>
-                <footer id="socials" style="position:fixed;bottom:0px"class="flex flex-row w-full p-4 md:w-1/2 justify-center fade-start">
-                    <a href="https://www.linkedin.com/in/dom-elves-681774200/">
-                        <i class="fa-brands fa-linkedin" style="font-size:35px"></i>
-                    </a>
-                    <div style="width:75px"></div>
-                    <a href="https://github.com/dom-elves/chopr">
-                        <i class="fa-brands fa-square-github" style="font-size:35px"></i>
-                    </a>
-                </footer>
-            </div>
-        </div>    
-    </div>
+    <GuestLayout>
+        <div id="links" class="flex flex-row w-full mt-10 justify-center fade-start">
+            <Link
+                :href="route('login')"
+                class="text-xl font-bold text-right uppercase rounded-md text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                style="width:110px;"
+            >
+                Log in
+            </Link>
+            <div style="width:32px;border-right: 1px solid black"></div>
+            <div style="width:32px;border-left: 1px solid black"></div>
+            <Link
+                v-if="canRegister"
+                :href="route('register')"
+                class="text-xl font-bold uppercase rounded-md text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                style="width:110px;"
+            >
+                Register
+            </Link>
+        </div>
+    </GuestLayout>
 </template>
-<style scoped>
-
-.fade-start {
-  opacity: 0;
-  transition: opacity 1s ease-in;
-}
-
-.fade-start.fade-in {
-  opacity: 1;
-}
-</style>
