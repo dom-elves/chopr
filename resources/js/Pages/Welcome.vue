743b92989911ffa686c31a4985c116812473828c
Dom Elves <dom_elves@hotmail.co.uk>
2025-11-05 10:09:19 +0000

    improved styling for welcome page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -29,31 +29,37 @@ defineProps({
     <Head title="Welcome" />
     <div class="bg-slate-100 w-full flex flex-col items-center">
         <header></header>
-        <div class="flex flex-col md:w-1/2 bg-white  min-h-screen">
-            <img src="/storage/chopr-logo-2.png" class="mt-20"/>
-            <main class="flex flex-row justify-evenly w-full mt-10">
-                <Link
-                    :href="route('login')"
-                    class="text-xl font-bold uppercase rounded-md text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                >
-                    Log in
-                </Link>
-                <Link
-                    v-if="canRegister"
-                    :href="route('register')"
-                    class="text-xl font-bold uppercase rounded-md text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                >
-                    Register
-                </Link>
-            </main>
-            <footer style="bottom:0px;position:fixed" class="flex flex-row p-4 w-full md:w-1/2 justify-evenly">
-                <a href="https://www.linkedin.com/in/dom-elves-681774200/">
-                    <i class="fa-brands fa-linkedin" style="font-size:35px"></i>
-                </a>
-                <a href="https://github.com/dom-elves/chopr">
-                    <i class="fa-brands fa-square-github" style="font-size:35px"></i>
-                </a>
-            </footer>
+        <div class="flex flex-col md:w-1/2 bg-white  min-h-screen items-center">
+            <div style="max-width:390px" class="flex flex-col items-center">
+                <img src="/storage/chopr-logo-2.png" class="mt-20"/>
+                <main class="flex flex-row w-full mt-10 justify-center">
+                    <Link
+                        :href="route('login')"
+                        class="text-xl font-bold text-right uppercase rounded-md text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                        style="width:110px;"
+                    >
+                        Log in
+                    </Link>
+                    <div style="width:75px"></div>
+                    <Link
+                        v-if="canRegister"
+                        :href="route('register')"
+                        class="text-xl font-bold uppercase rounded-md text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                        style="width:110px;"
+                    >
+                        Register
+                    </Link>
+                </main>
+                <footer style="position:fixed;bottom:0px"class="flex flex-row w-full p-4 md:w-1/2 justify-center">
+                    <a href="https://www.linkedin.com/in/dom-elves-681774200/">
+                        <i class="fa-brands fa-linkedin" style="font-size:35px"></i>
+                    </a>
+                    <div style="width:75px"></div>
+                    <a href="https://github.com/dom-elves/chopr">
+                        <i class="fa-brands fa-square-github" style="font-size:35px"></i>
+                    </a>
+                </footer>
+            </div>
         </div>    
     </div>
 </template>
