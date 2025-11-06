88f519e6cfaba5592f4c013ca8b6fef85cfc9094
Dom Elves <dom_elves@hotmail.co.uk>
2025-11-06 15:29:26 +0000

    alt enter button for users with session

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -30,7 +30,7 @@ onMounted(() => {
             <a href="/" style="max-width:576px">
                 <img src="/storage/chopr-logo-2.png" class="mt-20 t" id="title-image"/>
             </a>
-            <p id="beta" class=" font-bold">BETA</p>
+            <p id="beta" class="mb-10 font-bold">BETA</p>
             <!-- slot is things like registartion, pw recovery -->
             <div id="slot-content" class=" w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
                 <slot />
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -25,24 +25,35 @@ defineProps({
 <template>
     <Head title="Welcome" />
     <GuestLayout>
-        <div id="links" class="flex flex-row w-full mt-10 justify-center">
-            <Link
-                :href="route('login')"
-                class="text-xl font-bold text-right uppercase rounded-md text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                style="width:110px;"
-            >
-                Log in
-            </Link>
-            <div style="width:32px;border-right: 1px solid black"></div>
-            <div style="width:32px;border-left: 1px solid black"></div>
-            <Link
-                v-if="canRegister"
-                :href="route('register')"
-                class="text-xl font-bold uppercase rounded-md text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                style="width:110px;"
-            >
-                Register
-            </Link>
+        <div class="w-full justify-center" id="links">
+            <div v-if="$page.props.auth.user" style="text-align:center">
+                <Link
+                    :href="route('debt.index')"
+                    class="text-xl font-bold uppercase rounded-md text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                    style="width:110px;"
+                >
+                    Enter
+                </Link>
+            </div>
+            <div v-else class="flex flex-row justify-center">
+                <Link
+                    :href="route('login')"
+                    class="text-xl font-bold text-right uppercase rounded-md text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                    style="width:110px;"
+                >
+                    Log in
+                </Link>
+                <div style="width:32px;border-right: 1px solid black"></div>
+                <div style="width:32px;border-left: 1px solid black"></div>
+                <Link
+                    v-if="canRegister"
+                    :href="route('register')"
+                    class="text-xl font-bold uppercase rounded-md text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                    style="width:110px;"
+                >
+                    Register
+                </Link>
+            </div>
         </div>
     </GuestLayout>
 </template>
