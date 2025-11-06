8ca7ed6f4b04c2c9ea5dba4ed5032c957be31b4c
Dom Elves <dom_elves@hotmail.co.uk>
2025-11-06 12:02:24 +0000

    rework layout of comment component

--- resources/js/Components/Comments/Comment.vue
+++ resources/js/Components/Comments/Comment.vue
@@ -8,6 +8,7 @@ import PrimaryButton from '@/Components/Misc/PrimaryButton.vue';
 import SecondaryButton from '@/Components/Misc/SecondaryButton.vue';
 import InputError from '@/Components/Forms/InputError.vue';
 import DangerButton from '@/Components/Misc/DangerButton.vue';
+import UserProfileIcon from '@/Components/Users/UserProfileIcon.vue';
 
 const props = defineProps({
     comment: {
@@ -39,71 +40,71 @@ onMounted(() => {
 
 </script>
 <template>
-    <div class="flex flex-row items-center my-2 bg-gray-100" style="border-radius:15px;padding:10px">
-        <div class="flex flex-row w-full items-center">
-            <svg width="50" height="50" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="25" cy="25" r="20" stroke="green" stroke-width="4" fill="yellow" />
-            </svg>
-            <div class="flex-col w-full">
-                <!-- name & date -->
-                <div>
-                    <strong>{{ comment.user.name }}</strong>
-                    <small> on {{ formatCommentDate(comment.created_at) }}</small>
-                </div>
-                <i v-if="comment.edited">
-                    <small>last edited at {{ formatCommentDate(comment.updated_at) }}</small>
-                </i>
-                <!-- content & edit form -->
-                <div class="w-full">
-                    <p  v-if="!isEditing"class="p-2">{{ comment.content }}</p>
-                    <Form
-                        v-else
-                        :action="route('comment.update')"
-                        method="patch"
-                        resetOnSuccess
-                        :transform="data => ({ 
-                            ...data, 
-                            id: props.comment.id,
-                            debt_id: props.comment.debt_id,
-                            user_id: usePage().props.auth.user.id,
-                        })"
-                        :options="{
-                            preserveScroll: true,
-                        }"
-                        @success="isEditing = false"
-                    >   
-                        <label for="edit_comment" class="hidden">Edit comment</label>
-                        <textarea 
-                            class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-                            id="edit_comment"
-                            name="content"
-                        >
-                        </textarea>
-                        <div class="flex flex-row mt-2 sm:justify-end">
-                            <SecondaryButton
-                                type="button"
-                                class="mr-2"
-                                @click="isEditing = false"
-                            >
-                                Cancel
-                            </SecondaryButton>
-                            <PrimaryButton
-                                type="submit"
-                            >
-                                Save
-                            </PrimaryButton>
-                        </div>
-                    </Form>
+    <div class="flex flex-col items-center my-2 bg-gray-100" style="border-radius:15px;padding:10px">
+        <!-- picture, name, controls-->
+        <div class="flex flex-row w-full justify-between">
+            <div class="flex flex-row">
+                <UserProfileIcon class="mr-2"/>
+                <div class="flex flex-col">
+                    <div>
+                        <strong>{{ comment.user.name }}</strong>
+                        <small> on {{ formatCommentDate(comment.created_at) }}</small>
+                    </div>
+                    <i v-if="comment.edited">
+                        <small>last edited at {{ formatCommentDate(comment.updated_at) }}</small>
+                    </i>
                 </div>
             </div>
+            <Controls
+                v-if="usePage().props.ownership.comment_ids.includes(props.comment.id)"
+                item="Comment"
+                @edit="isEditing = !isEditing"
+                @destroy="confirmingCommentDeletion = true"
+            >
+            </Controls>
         </div>
-        <Controls
-            v-if="usePage().props.ownership.comment_ids.includes(props.comment.id)"
-            item="Comment"
-            @edit="isEditing = !isEditing"
-            @destroy="confirmingCommentDeletion = true"
-        >
-        </Controls>
+        <!-- comment & edit form -->
+        <div class="w-full">
+            <p  v-if="!isEditing"class="p-2">{{ comment.content }}</p>
+            <Form
+                v-else
+                :action="route('comment.update')"
+                method="patch"
+                resetOnSuccess
+                :transform="data => ({ 
+                    ...data, 
+                    id: props.comment.id,
+                    debt_id: props.comment.debt_id,
+                    user_id: usePage().props.auth.user.id,
+                })"
+                :options="{
+                    preserveScroll: true,
+                }"
+                @success="isEditing = false"
+            >   
+                <label for="edit_comment" class="hidden">Edit comment</label>
+                <textarea 
+                    class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                    id="edit_comment"
+                    name="content"
+                >
+                </textarea>
+                <div class="flex flex-row mt-2 sm:justify-end">
+                    <SecondaryButton
+                        type="button"
+                        class="mr-2"
+                        @click="isEditing = false"
+                    >
+                        Cancel
+                    </SecondaryButton>
+                    <PrimaryButton
+                        type="submit"
+                    >
+                        Save
+                    </PrimaryButton>
+                </div>
+            </Form>
+        </div>
         <Modal :show="confirmingCommentDeletion" @close="closeModal">
             <div class="p-6 flex flex-col">
                 <h2
