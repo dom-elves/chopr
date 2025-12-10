cb39c8807a1c7c2daabc7a438dbf0b5e49ac71a2
Dom Elves <dom_elves@hotmail.co.uk>
2025-12-10 11:18:31 +0000

    clarity changes for add debt form share & amount, currently works only with non split even debts, full explanation will be in PR

--- resources/js/Components/Debts/AddDebt/Form/AddDebtFormAmount.vue
+++ resources/js/Components/Debts/AddDebt/Form/AddDebtFormAmount.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 import { store } from '@/debt.js';
 import InputError from '@/Components/Forms/InputError.vue';
+import Slider from '@/Components/Misc/Slider.vue';
 
 // props
 const props = defineProps({
@@ -10,31 +11,73 @@ const props = defineProps({
     },
 });
 
-onMounted(() => {
+// same principle as in AddDebtFormShare, make total a string for user's benefit
+const userFacingTotalValue = ref('');
 
+function setAmount() {
+    if (store.addDebtForm.split_even) {
+        store.splitEven(userFacingTotalValue.value);
+    } else if (!store.addDebtForm.split_even) {
+        store.calcTotalAmount();
+    }
+}
+
+/**
+ * Toggles between the debt being split even/custom shares. Basically looks at total_amount if split, 
+ * and then indiviudal fields if not. 
+ */
+function toggleSplitEven(toggle) {
+    store.addDebtForm.split_even = toggle;
+
+    if (store.addDebtForm.split_even) {
+        store.splitEven();
+    } else {
+        store.calcTotalAmount()
+    }
+}
+
+onMounted(() => {
+    console.log('f', store.addDebtForm);
 })
+
+// sort of inverse to how it works on shares
+// as shares take a string and make a number for the sake of the form
+// the total takes the number from the form and makes a string with decimal point
+watch(() => store.addDebtForm.amount, (newAmount) => {
+    const majorUnits = String(newAmount).slice(0, -2) || '0';
+    const minorUnits = String(newAmount).slice(-2) || '00';
+    userFacingTotalValue.value = majorUnits + '.' + minorUnits;
+});
 
 </script>
 <template>
-    <div>
-        <label 
-            for="debt-amount" 
-            class="block text-sm font-medium text-gray-700 hidden"
-            id="debtamount"
+    <div class="flex flex-row justify-between mt-2">
+        <div>
+            <label 
+                for="debt-amount" 
+                class="block text-sm font-medium text-gray-700"
+                id="debt-amount"
+            >
+                Amount:
+            </label>
+            <input
+                v-model="userFacingTotalValue" 
+                type="text"
+                id="debt-amount" 
+                amount="debt-amount" 
+                aria-labelledby="debt-amount"
+                @change="setAmount"
+                :disabled="!store.addDebtForm.split_even"
+                class="rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+            />
+        </div>
+        <Slider
+            label="Split even"
+            alignment="end"
+            size="xs"
+            @toggled="toggleSplitEven"
         >
-            Debt Amount
-        </label>
-        <input
-            v-model="store.addDebtForm.amount" 
-            type="number"
-            step="0.01" 
-            id="debt-amount" 
-            amount="debt-amount" 
-            aria-labelledby="debtAmount"
-            @change=store.splitEven()
-            :disabled="!store.addDebtForm.split_even"
-            class="rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-        />
+        </Slider>
         <InputError class="mt-2" :message="errors" />
     </div>
 </template>
--- resources/js/Components/Debts/AddDebt/Form/AddDebtFormShare.vue
+++ resources/js/Components/Debts/AddDebt/Form/AddDebtFormShare.vue
@@ -4,56 +4,64 @@ import { store } from '@/debt.js';
 import Slider from '@/Components/Misc/Slider.vue';
 import TextInput from '@/Components/Forms/TextInput.vue';
 import Dinero from 'dinero.js'
+import InputError from '@/Components/Forms/InputError.vue';
 
 // props
 const props = defineProps({
     group_user: {
         type: Object,
     },
+    errors: {
+        type: Object,
+        required: false,
+    },
 });
 
+// because we have to mess around with string/int casting
+// show the user a string of the share with a decimal point
+const userFacingShareValue = ref('');
+// ref for changing css on share input element
+const isShareValid = ref(true);
+// the share for the user, taken from the store
 const share = ref(store.addDebtForm.user_shares.find((userShare) => 
     userShare.user_id == props.group_user.user_id
 ));
 
-// separate vars for major/minor units
-const majorUnits = ref(0);
-const minorUnits = ref(0);
-
+// check validity of share
+// if invalid, set it back to 0 and uncheck
+// otherwise, check & cast to number so it can be calced
 function setShareAmount() {
-    // if the user is interacting with either input
-    // set it as checked
-    share.value.checked = true;
+    isShareValid.value = validateShare();
+    
+    if (!isShareValid.value) {
 
-    // removing 0s leads to input being empty string, to manually set to 0
-    if (majorUnits.value == '') {
-        majorUnits.value = 0;
-    }
-
-    // same as above
-    if (minorUnits.value == '') {
-        minorUnits.value = 0;
-    }
-
-    // non strict comparison to allow major and minor units being 0
-    // uncheck if both are falsey
-    if (majorUnits.value == 0 && minorUnits.value == 0) {
         share.value.checked = false;
         share.value.amount = 0;
+    } else {
+
+        share.value.checked = true;
+        share.value.amount = Number(userFacingShareValue.value.replace('.', '')); 
     }
 
-    // recalc with entire amount in minor units, e.g. £12.34 = 1234
-    share.value.amount = majorUnits.value * 100 + minorUnits.value;
     store.calcTotalAmount();
 }
 
+// check the validity of user input when attempting to add a share
+// regex is for a 9 digit number with 2 decimal places
+// specifically including the decimal point
+function validateShare() {
+    const regex = new RegExp("^\\d{1,9}\\.\\d{2}$");
+    return regex.test(userFacingShareValue.value);
+}
+
+// just toggles if a split even share is checked
 function toggleShareChecked(toggle) {
     share.value.checked = toggle;
 
     store.addDebtForm.user_shares.find((userShare) => 
         userShare.user_id == share.value.user_id).checked = share.value.checked;
 
-    store.splitEven();
+    store.splitEven(store.addDebtForm.amount);
 }
 
 onMounted(() => {
@@ -87,54 +95,36 @@ onMounted(() => {
             <div class="flex flex-row justify-center items-center">
                 <label 
                     :for="`share-major-amount-${group_user.id}`"
-                    class="hidden"
+                   class="mr-1"
                 >
-                    Amount
+                    £
                 </label>
                 <input 
-                    type="number"
+                    type="text"
                     :id="`share-major-amount-${group_user.id}`"
                     :name="`share-major-amount-${group_user.id}`"
-                    v-model="majorUnits"
+                    v-model="userFacingShareValue"
                     @change="setShareAmount"
                     :disabled="store.addDebtForm.split_even"
-                    class="w-1/2 md:w-24 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 mr-2"
-                >
-                <label 
-                    :for="`share-minor-amount-${group_user.id}`"
-                    class="hidden"
-                >
-                    Amount
-                </label>
-                <input 
-                    type="number"
-                    :id="`share-minor-amount-${group_user.id}`"
-                    :name="`share-minor-amount-${group_user.id}`"
-                    v-model="minorUnits"
-                    @change="setShareAmount"
-                    :disabled="store.addDebtForm.split_even"
-                    class="w-1/2 md:w-24 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 mr-2"
-                    maxlength="2"
+                    class="w-1/2 md:w-24 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 mr-1"
+                    :class="isShareValid ? '' : 'bg-red-200'"
+                    maxlength="9"
+                    inputmode="decimal"
+                    placeholder="0.00"
                 >
                 <div :class="store.addDebtForm.split_even ? '' : 'invisible'">
-                <Slider
-                    @toggled="toggleShareChecked"
-                    :checked="share.checked"
-                    alignment="end"
-                >
-                </Slider>
+                    <Slider
+                        @toggled="toggleShareChecked"
+                        :checked="share.checked"
+                        alignment="end"
+                    >
+                    </Slider>
                 </div>
             </div>
         </div>
+        <InputError class="mt-2" :message="errors" />
     </div>
 </template>
 <style scoped>
 
-textarea {
-    resize: none;
-}
-
-textarea:invalid {
-  border: 2px solid red;
-}
 </style>
