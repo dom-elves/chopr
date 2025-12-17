50e9a223607798b187156d5f024cc9c5390336fa
Dom Elves <dom_elves@hotmail.co.uk>
2025-12-17 20:49:21 +0000

    resources for alias, group, group user and user and controller updates

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Validator;
 use App\Rules\IsGroupOwner;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\GroupResource;
 
 class GroupController extends Controller
 {
@@ -24,19 +25,14 @@ class GroupController extends Controller
     public function index(Request $request)
     {
         $user = Auth::user();
-        $groups = Inertia::scroll(fn() => $request->user()
-            ->groups()
-            ->with(['group_users.user', 'group_users.aliases'])
-            ->paginate(5)
-            ->through(fn ($group) => [
-                ...$group->toArray(),
-                'can' => [
-                    'update' => $user->can('update', $group),
-                    'invite' => $user->can('invite', $group),
-                    'delete' => $user->can('delete', $group),
-                ],
-            ]
-        ));
+        $groups = Inertia::scroll(fn () =>
+            GroupResource::collection(
+                $request->user()
+                    ->groups()
+                    ->with(['group_users.user', 'group_users.aliases'])
+                    ->paginate(5)
+                )
+            );
 
         return Inertia::render('Groups', [
             'groups' => $groups,
---
+++ app/Http/Resources/GroupResource.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\GroupUserResource;
+
+class GroupResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'user_id' => $this->user_id,
+
+            // permissions
+            'can' => [
+                'update' => $request->user()->can('update', $this->resource),
+                'invite' => $request->user()->can('invite', $this->resource),
+                'delete' => $request->user()->can('delete', $this->resource),
+            ],
+
+            // relationships
+            // over time, more of these will likely be filled out
+            // as & when  more relationships are needed
+            'group_users' => GroupUserResource::collection(
+                $this->whenLoaded('group_users')
+            ),
+        ];
+    }
+}
