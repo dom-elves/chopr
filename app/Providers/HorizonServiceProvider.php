59a63bbe8c61471f5c7910aa8de90963f37c5e0a
Dom Elves <dom_elves@hotmail.co.uk>
2026-01-28 16:07:19 +0000

    add self to permitted horizon viewers

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -29,7 +29,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     {
         Gate::define('viewHorizon', function ($user = null) {
             return in_array(optional($user)->email, [
-                //
+                'dom_elves@hotmail.co.uk'
             ]);
         });
     }
