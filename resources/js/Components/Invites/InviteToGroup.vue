8b1ede03f39498abd514cf7db7c3b7590b2ef48a
Dom Elves <dom_elves@hotmail.co.uk>
2026-01-30 14:38:25 +0000

    fix tests & separate frontend errors

--- resources/js/Components/Invites/InviteToGroup.vue
+++ resources/js/Components/Invites/InviteToGroup.vue
@@ -57,100 +57,101 @@ function removeRecipient(emailAddress) {
             :closeable="true" 
             @close="openModal = !openModal"
         >     
-        <div class="p-6">
-            <!-- input for building email array -->
-            <div class="mb-4 w-full">
-                <label
-                    for="recipient"
-                    class="text-lg font-medium text-gray-900"
-                >
-                    Enter the addresses of who you wish to invite:
-                </label>
-                <input
-                    id="recipient"
-                    class="rounded-l-md border-gray-300 shadow-sm"
-                    type="email"
-                    name="recipient"
-                    v-model="recipient"
-                    @keydown.enter.prevent="addRecipient($event.target.value)"
-                    placeholder="Enter email & press enter"
-                    style="border-right:none;width:calc(100% - 37px)"
-                >
-                <button
-                    type="button"
-                    class="px-2 rounded-r-md border border-gray-300 shadow-sm"
-                    style="height:42px;border-left:none"
-                    @click="recipient = ''"
-                    >
-                    <i
-                        class="fa-solid fa-x mx-1 fa-xs"
-                    ></i>
-                </button>
-            </div>
-            <InputError class="mt-2" :message="recipientError" />
-            <!-- actual form for submission -->
-            <Form
-                :action="route('invite.send')" 
-                method="post" 
-                #default="{ errors }"
-                @success="openModal = false"
-                resetOnSuccess
-                :transform="data => ({ 
-                    ...data, 
-                    recipients: recipients,
-                    group_id: props.group.id,
-                    user_id: usePage().props.auth.user.id,
-                })"
-            >
-                <!-- recipient badges -->
-                <div class="mb-4">
-                    <span 
-                        v-for="recipient in recipients"
-                        class="items-center rounded-md border border-black p-1 bg-gray-900 text-white font-semibold my-1 mr-1"
-                        style="display:inline-block;word-break:break-word"
-                        >
-                        {{ recipient }}
-                        <i
-                            class="fa-solid fa-x mx-1 fa-xs"
-                            @click="removeRecipient(recipient)"
-                        ></i>
-                    </span>
-                </div>
-                <!-- just so this appears to be an error for the mail input field -->
-                <div v-for="(error, key) in errors" class="my-2">
-                    <InputError v-if="key != 'body'" :message="error" />
-                </div>
-                <!-- message -->
-                <div class="mb-4">
+            <div class="p-6">
+                <!-- input for building email array -->
+                <div class="mb-4 w-full">
                     <label
+                        for="recipient"
                         class="text-lg font-medium text-gray-900"
                     >
-                        And a message for them:
+                        Enter the addresses of who you wish to invite:
                     </label>
-                    <textarea
-                        class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-                        type="text"
-                        name="body"
-                        placeholder="Add a message to your invite"
+                    <input
+                        id="recipient"
+                        class="rounded-l-md border-gray-300 shadow-sm"
+                        type="email"
+                        name="recipient"
+                        v-model="recipient"
+                        @keydown.enter.prevent="addRecipient($event.target.value)"
+                        placeholder="Enter email & press enter"
+                        style="border-right:none;width:calc(100% - 37px)"
                     >
-                    </textarea>
-                    <InputError class="mt-2" v-if="errors.body" :message="errors.body" />
-                </div>
-                <div class="flex flex-row mt-4 justify-center sm:justify-end w-full">
-                    <SecondaryButton 
+                    <button
                         type="button"
-                        @click="openModal = false"
-                    >
-                        Cancel
-                    </SecondaryButton>
-                    <PrimaryButton
-                        type="submit"
-                    >
-                        Send
-                    </PrimaryButton>
+                        class="px-2 rounded-r-md border border-gray-300 shadow-sm"
+                        style="height:42px;border-left:none"
+                        @click="recipient = ''"
+                        >
+                        <i
+                            class="fa-solid fa-x mx-1 fa-xs"
+                        ></i>
+                    </button>
                 </div>
-            </Form>
-        </div>
+                <InputError class="mt-2" :message="recipientError" />
+                <!-- actual form for submission -->
+                <Form
+                    :action="route('invite.send')" 
+                    method="post" 
+                    #default="{ errors }"
+                    @success="openModal = false"
+                    resetOnSuccess
+                    :transform="data => ({ 
+                        ...data, 
+                        recipients: recipients,
+                        group_id: props.group.id,
+                        user_id: usePage().props.auth.user.id,
+                    })"
+                >
+                    <!-- recipient badges -->
+                    <div class="mb-4">
+                        <span 
+                            v-for="recipient in recipients"
+                            class="items-center rounded-md border border-black p-1 bg-gray-900 text-white font-semibold my-1 mr-1"
+                            style="display:inline-block;word-break:break-word"
+                            >
+                            {{ recipient }}
+                            <i
+                                class="fa-solid fa-x mx-1 fa-xs"
+                                @click="removeRecipient(recipient)"
+                            ></i>
+                        </span>
+                    </div>
+                    <!-- recipient errors -->
+                    <div class="my-2">
+                        <InputError v-if="errors.pending" :message="errors.pending" />
+                        <InputError v-if="errors.existing" :message="errors.existing" />
+                    </div>
+                    <!-- message -->
+                    <div class="mb-4">
+                        <label
+                            class="text-lg font-medium text-gray-900"
+                        >
+                            And a message for them:
+                        </label>
+                        <textarea
+                            class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                            type="text"
+                            name="body"
+                            placeholder="Add a message to your invite"
+                        >
+                        </textarea>
+                        <InputError class="mt-2" v-if="errors.body" :message="errors.body" />
+                    </div>
+                    <div class="flex flex-row mt-4 justify-center sm:justify-end w-full">
+                        <SecondaryButton 
+                            type="button"
+                            @click="openModal = false"
+                        >
+                            Cancel
+                        </SecondaryButton>
+                        <PrimaryButton
+                            type="submit"
+                        >
+                            Send
+                        </PrimaryButton>
+                    </div>
+                </Form>
+            </div>
         </Modal>
     </div>
 </template>
