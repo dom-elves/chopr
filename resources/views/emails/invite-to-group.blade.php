5242deb4625c50dd75a92daea020474db321b808
Dom Elves <dom_elves@hotmail.co.uk>
2026-02-02 15:46:33 +0000

    add temporary signed route to invite mail, use 'signed' middleware, add signed route to mail

--- resources/views/emails/invite-to-group.blade.php
+++ resources/views/emails/invite-to-group.blade.php
@@ -2,7 +2,6 @@
     <p>{{ $invite->user->name }} has invited you to join the group, <i>{{ $invite->group->name }}</i>, on chopr:</p>
     <p><i>{{ $invite->body }}</i></p>
 
-    <a href="{{ route('invite.accept', ['token' => $invite->token]) }}">
-        {{ $is_new_user ? 'Sign up' : 'Join' }}
-    </a>
+  
+    {{ $link }}
 </div>
