6e418852b77a49f9f4279ee21bf13cafbbfd86d3
Dom Elves <domelves@Doms-MacBook-Air.local>
2026-02-10 20:32:05 +0000

    forgotten ternary for when no user is logged in

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -42,7 +42,7 @@ class HandleInertiaRequests extends Middleware
             'flash' => [
                 'status' => fn () => $request->session()->get('status'),
             ],
-            'notifications' => $user->unreadNotifications,
+            'notifications' => $user ? $user->unreadNotifications : [],
         ]);
     }
 }
