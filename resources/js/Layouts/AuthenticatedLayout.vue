9caae22e3421dc9d480c5489e6fb7cbe23522709
Dom Elves <dom_elves@hotmail.co.uk>
2026-02-11 10:41:28 +0000

    condition for when there are no notifications

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -189,7 +189,12 @@ const showingNavigationDropdown = ref(false);
                                         Notifications
                                     </button>
                                 </template>
-                                <template #content>
+                                <template #content v-if="notifications.length === 0">
+                                    <p class="block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:bg-gray-100 focus:outline-none">
+                                        You have no new notifications
+                                    </p>
+                                </template>
+                                <template #content v-else>
                                     <Notification v-for="notification in notifications"
                                         :notification="notification"
                                     >
