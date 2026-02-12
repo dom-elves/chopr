78e73aa94a7d714922ca6839f297b440b47917df
Dom Elves <dom_elves@hotmail.co.uk>
2026-02-12 10:45:18 +0000

    div for 50%vh & scroll on notifs

--- resources/js/Components/Notifications/Notifications.vue
+++ resources/js/Components/Notifications/Notifications.vue
@@ -48,7 +48,7 @@ function readNotification(notificationId) {
                 class="text-center inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none"
             >
                 <i class="fa-solid fa-circle-exclamation text-2xl text-gray-400"></i>
-                <p class="flex justify-center text-white bg-red-500 p-1 rounded-full text-center" style="height:25px;width:25px;position:relative;right:10px;bottom:10px" :class="notifications.length > 0 ? 'visible' : 'invisible'">{{ notifications.length }}</p>
+                <p class="text-white bg-red-500 p-1 rounded-full text-center" style="height:25px;width:25px;position:relative;right:10px;bottom:10px" :class="notifications.length > 0 ? 'visible' : 'invisible'">{{ notifications.length }}</p>
             </button>
         </template>
         <template #content v-if="notifications.length === 0">
@@ -57,11 +57,13 @@ function readNotification(notificationId) {
             </p>
         </template>
         <template #content v-else>
-            <Notification v-for="notification in notifications"
-                :notification="notification"
-                @notificationRead="readNotification"
-            >
-            </Notification>
+            <div style="max-height:50vh;overflow-y:scroll;">
+                <Notification v-for="notification in notifications"
+                    :notification="notification"
+                    @notificationRead="readNotification"
+                >
+                </Notification>
+            </div>
             <button 
                 class="block w-full px-4 py-2 text-center text-sm leading-5 text-blue-700 transition duration-150 ease-in-out hover:underline hover:cursor-pointer hover:bg-gray-100 focus:bg-gray-100 focus:outline-none"
                 @click="readAllNotifications"
