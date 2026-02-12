ac733ec9e108cc0228fb6d28c66563d71bb980ed
Dom Elves <dom_elves@hotmail.co.uk>
2026-02-12 11:30:17 +0000

    computed icon for notif type

--- resources/js/Components/Notifications/Notification.vue
+++ resources/js/Components/Notifications/Notification.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted, watch } from 'vue';
+import { ref, onMounted, watch, computed } from 'vue';
 import { usePage, router } from '@inertiajs/vue3';
 
 const emit = defineEmits(['notificationRead']);
@@ -24,8 +24,20 @@ function readNotification() {
     )
 }
 
+const iconClass = computed(() => {
+    const type = props.notification.type;
+    
+    if (type.includes('Debt')) {
+        return 'fa-list text-green-300';
+    } else if (type.includes('Group')) {
+        return 'fa-users text-blue-300';
+    } else if (type.includes('Share')) {
+        return 'fa-chart-pie text-orange-300';
+    }
+});
+
 onMounted(() => {
-    console.log(props.notification.type);
+    console.log(iconClass.value);
 })
 
 </script>
@@ -33,7 +45,7 @@ onMounted(() => {
 <template>
     <div class="flex flex-row items-center w-full py-2 text-start text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:bg-gray-100 focus:outline-none">
         
-        <i class="fa-solid fa-x px-4"></i>
+        <i class="fa-solid px-4" :class="iconClass"></i>
         <p>
             You have been added to a debt of <b>£{{ notification.data.amount.amount }}</b> for <b>{{ notification.data.name }}</b> in {{ notification.data.group_name }} by <b>{{ notification.data.owner.name }}</b>
         </p>
