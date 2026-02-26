5073acdd44e046a2d8d68d80502087fd4d436636
Dom Elves <dom_elves@hotmail.co.uk>
2026-02-26 15:17:02 +0000

    refactor computed mobile notif classes

--- resources/js/Components/Notifications/MobileNotifications.vue
+++ resources/js/Components/Notifications/MobileNotifications.vue
@@ -11,14 +11,18 @@ const active = ref(false);
 // stolen from ResponsiveNavLink
 const classes = computed(() =>
     active.value
-        ? 'block w-full ps-3 pe-4 py-2 border-l-4 border-indigo-400 text-start text-base font-medium text-indigo-700 bg-indigo-50 focus:outline-none focus:text-indigo-800 focus:bg-indigo-100 focus:border-indigo-700 transition duration-150 ease-in-out'
-        : 'block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out',
+        ? 'border-indigo-400 text-indigo-700 bg-indigo-50 focus:text-indigo-800 focus:bg-indigo-100 focus:border-indigo-700'
+        : 'border-transparent text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300',
 );
 
 </script>
 <template>
     <div>
-        <div class="flex flex-row items-center justify-between" :class="classes" @click="active = !active">
+        <div 
+            class="flex flex-row items-center justify-between text-start text-base font-medium focus:outline-none transition duration-150 ease-in-out block w-full ps-3 pe-4 py-2 border-l-4" 
+            :class="classes" 
+            @click="active = !active"
+        >
             <p>Notifications</p>
             <div
                 class="bg-red-500 rounded-full flex items-center justify-center" 
