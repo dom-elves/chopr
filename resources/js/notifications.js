ca105e9797b73a34352bd0f7c1317ca836bdd639
Dom Elves <dom_elves@hotmail.co.uk>
2026-02-26 11:38:47 +0000

    move notif component structure to separate js file as it will likely become massive in the future

---
+++ resources/js/notifications.js
@@ -0,0 +1,10 @@
+export const notificationTypes = {
+    DebtCreatedNotification: (notificationData) => ({
+        classes : 'fa-list text-green-300',
+        message: `You have been added to a debt of £${notificationData.amount.amount} for ${notificationData.name} in ${notificationData.group_name} by ${notificationData.owner.name}`
+    }),
+    GroupCreatedNotification: (notificationData) => ({
+        classes : 'fa-users text-blue-300',
+        message: `You have been added to the group ${notificationData.name} by ${notificationData.group_owner}`
+    }),
+}
