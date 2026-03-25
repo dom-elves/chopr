3d7841196d4430239bffa183326536383d872036
Dom Elves <dom_elves@hotmail.co.uk>
2026-03-25 14:21:10 +0000

    change user relationship from group_user to groupUser

--- app/Http/Controllers/DebtController.php
+++ app/Http/Controllers/DebtController.php
@@ -30,7 +30,7 @@ class DebtController extends Controller
             DebtResource::collection(
                 // query builder to get the debts where the user is the owner
                 // or has a share in the debt via group_user
-                Debt::whereIn('group_user_id', $user->group_users->pluck('id')->toArray())
+                Debt::whereIn('group_user_id', $user->groupUsers->pluck('id')->toArray())
                     ->orWhereHas('shares.group_user', function ($query) use ($user) {
                         $query->where('user_id', $user->id);
                     })
