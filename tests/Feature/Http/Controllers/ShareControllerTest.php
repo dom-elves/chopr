7c6d1772ca2f823b1a43e969e938fe14db4846b9
Dom Elves <dom_elves@hotmail.co.uk>
2026-04-29 11:21:14 +0100

    tests around share names, adding and deleting shares to debts they dont own

--- tests/Feature/Http/Controllers/ShareControllerTest.php
+++ tests/Feature/Http/Controllers/ShareControllerTest.php
@@ -46,13 +46,16 @@ beforeEach(function () {
  * Tests for shared behaviours where the debt type is irrelevant
  * - Select 'sent' on own share
  * - Select 'seen' on own debt share that's not their share
- * - Can not select 'seen' on a share they own that has not been sent
  * - Can not select 'sent' on a share they don't own
  * - Can not select 'seen' on a share they don't own
+ * - Can not select 'seen' on a share they own, but has not been sent
+ * - Can not select 'seen' on a share they do own, for a debt they do not own
  * - Update name on own share
  * - Not update on name on share they don't own and a debt they don't own
  * - Update name on a share they don't own, but a debt they do own
  * - Can not add a share without an amount
+ * - Can not add a share to a debt they do not own
+ * - Can not delete a share for a debt they do not own
  */
 test('user can select sent on their own share', function() {
     $response = $this->patch(route('share.sent', $this->share), [
@@ -175,6 +178,132 @@ test('user can not select seen on a share they do own for a debt they do not own
     ]);
 });
 
+test('user can update the name of their own share', function() {
+    $response = $this->patch(route('share.update', $this->share), [
+        'id' => $this->share->id,
+        'name' => 'i have been updated successfully',
+        'amount' => $this->share->amount->getMinorAmount()->toInt(),
+    ]);
+
+    $response->assertStatus(302)
+        ->assertSessionHasNoErrors()
+        ->assertSessionHas('status', 'Share updated successfully.');
+
+    $this->assertDatabaseHas('shares', [
+        'id' => $this->share->id,
+        'name' => 'i have been updated successfully',
+    ]);
+});
+
+test('user can not update a name of a share they do not own in a debt they do not own', function() {
+    $debt = Debt::factory()->withShares()->create([
+        'group_user_id' => $this->group_users->last()->id,
+        'group_id' => $this->group->id,
+    ]);
+
+    $share = $debt->shares->reject(fn($share) => 
+        $share->group_user_id === $this->group_user->id)->first();
+
+    $response = $this->patch(route('share.update', $share), [
+        'id' => $share->id,
+        'name' => 'i have been updated successfully',
+        'amount' => $share->amount->getMinorAmount()->toInt(),
+    ]);
+
+    $response->assertStatus(302)
+        ->assertSessionHasErrors('share', 'You do not have permission to update this share.');
+
+    $this->assertDatabaseHas('shares', [
+        'id' => $share->id,
+        'name' => $share->name,
+    ]);
+});
+
+test('user can update the name of a share they do not own in a debt they do own', function() {
+    $share = $this->debt->shares->reject(fn($share) => 
+        $share->group_user_id === $this->group_user->id)->first();
+
+    $response = $this->patch(route('share.update', $share), [
+        'id' => $share->id,
+        'name' => 'i have been updated successfully',
+        'amount' => $share->amount->getMinorAmount()->toInt(),
+    ]);
+
+    $response->assertStatus(302)
+        ->assertSessionHasNoErrors()
+        ->assertSessionHas('status', 'Share updated successfully.');
+
+    $this->assertDatabaseHas('shares', [
+        'id' => $share->id,
+        'name' => 'i have been updated successfully',
+    ]);
+});
+
+test('user can not add a share without an amount', function() {
+    $response = $this->post(route('share.store'), [
+        'debt_id' => $this->debt->id,
+        'group_user_id' => $this->group_user->id,
+        'name' => 'new share',
+        'currency' => 'GBP',
+    ]);
+
+    $response->assertStatus(302)
+        ->assertSessionHasErrors('amount', 'The amount field is required.');
+
+    $this->assertDatabaseMissing('shares', [
+        'debt_id' => $this->debt->id,
+        'group_user_id' => $this->group_user->id,
+        'name' => 'new share',
+    ]);
+});
+
+test('user can not add a share to a debt they do not own', function() {
+    $debt = Debt::factory()->withShares()->create([
+        'group_user_id' => $this->group_users->last()->id,
+        'group_id' => $this->group->id,
+    ]);
+
+    $response = $this->post(route('share.store'), [
+        'debt_id' => $debt->id,
+        'group_user_id' => $this->group_user->id,
+        'amount' => 500,
+        'name' => 'new share',
+        'currency' => 'GBP',
+    ]);
+
+    $response->assertStatus(302)
+        ->assertSessionHasErrors('debt_id', 'You do not have permission to add a share to this debt.');
+
+    $this->assertDatabaseMissing('shares', [
+        'debt_id' => $debt->id,
+        'group_user_id' => $this->group_user->id,
+        'name' => 'new share',
+    ]);
+});
+
+test('user can not delete a share for a debt they do not own', function() {
+    $debt = Debt::factory()->withShares()->create([
+        'group_user_id' => $this->group_users->last()->id,
+        'group_id' => $this->group->id,
+    ]);
+
+    $share = $debt->shares->reject(fn($share) =>
+        $share->group_user_id === $this->group_user->id)->first();
+
+    $response = $this->delete(route('share.destroy', $share));
+
+    $response->assertStatus(302)
+        ->assertSessionHasErrors('id', 'You do not have permission to delete this share.');
+
+    $this->assertDatabaseHas('shares', [
+        'id' => $share->id,
+        'group_user_id' => $share->group_user_id,
+        'debt_id' => $share->debt_id,
+        'deleted_at' => null,
+    ]);
+});
+
+
 // test("user can delete a share for a debt they own", function() {
 //     $debt = Debt::factory()->withShares()->create([
 //         'group_user_id' => $this->group_user->id,
@@ -201,36 +330,6 @@ test('user can not select seen on a share they do own for a debt they do not own
 //     $this->assertDatabaseHas('debts', [
 //         'id' => $debt->id,
 //         'amount' => ($debt->amount->minus($share->amount))->getMinorAmount()->toInt(),
-//     ]);
-// });
-
-// test("user can update the name on a share for a debt they own", function() {
-//     $debt = Debt::factory()->withShares()->create([
-//         'group_user_id' => $this->group_user->id,
-//         'group_id' => $this->group->id,
-//     ]);
-    
-//     $share = Share::factory()->create([
-//         'group_user_id' => $this->group_user->id,
-//         'debt_id' => $debt->id,
-//         'amount' => 500,
-//     ]);
-
-//     $response = $this->patch(route('share.update', $share), [
-//         'id' => $share->id,
-//         'debt_id' => $debt->id,
-//         'name' => 'new name for this share',
-//         'amount' => $share->amount->getMinorAmount()->toInt(),
-//     ]);
-
-//     $response->assertStatus(302)
-//         ->assertSessionHas('status', 'Share updated successfully.')
-//         ->assertSessionHasNoErrors();
-
-//     $this->assertDatabaseHas('shares', [
-//         'id' => $share->id,
-//         'group_user_id' => $share->group_user_id,
-//         'name' => 'new name for this share'
 //     ]);
 // });
 
@@ -341,72 +440,4 @@ test('user can not select seen on a share they do own for a debt they do not own
 //  * are hidden from users behind js on the Controls component
 //  */
 
-// test("user can not add a share for a debt they do not own", function() {
-//     $debt = Debt::factory()->withShares()->create([
-//         'group_user_id' => $this->group_users->last()->id,
-//         'group_id' => $this->group->id,
-//     ]);
 
-//     $response = $this->post(route('share.store'), [
-//         'debt_id' => $debt->id,
-//         'group_user_id' => $this->group_user->id,
-//         'amount' => 500,
-//         'name' => 'new share',
-//         'currency' => 'GBP',
-//     ]);
-
-//     $response->assertStatus(302)
-//         ->assertSessionHasErrors('debt_id', 'You do not have permission to add a share to this debt.');
-
-//     $this->assertDatabaseMissing('shares', [
-//         'debt_id' => $debt->id,
-//         'group_user_id' => $this->group_user->id,
-//         'amount' => 500,
-//     ]);
-// });
-
-
-// test("user can not delete a share for a debt they do not own", function() {
-//     $debt = Debt::factory()->withShares()->create([
-//         'group_user_id' => $this->group_users->last()->id,
-//         'group_id' => $this->group->id,
-//     ]);
-
-//     $share = $debt->shares->first();
-
-//     $response = $this->delete(route('share.destroy', $share));
-
-//     // check is against debt id
-//     $response->assertSessionHasErrors('id', 'You do not have permission to delete this share.');
-
-//     $this->assertDatabaseHas('shares', [
-//         'id' => $share->id,
-//         'deleted_at' => null,
-//     ]);
-// });
-
-// test("user can not update the name or amount on a share for a debt they do not own", function() {
-//     $debt = Debt::factory()->withShares()->create([
-//         'group_user_id' => $this->group_users->last()->id,
-//         'group_id' => $this->group->id,
-//     ]);
-    
-//     $share = $debt->shares->reject(fn($share) => 
-//         $share->group_user_id === $this->group_user->id)->first();
- 
-//     $new = $share->amount->plus(Money::of(10, 'GBP'));
-
-//     $response = $this->patch(route('share.update', $share), [
-//         'id' => $share->id,
-//         'debt_id' => $debt->id,
-//         'amount' => $new->getMinorAmount()->toInt(),
-//         'name' => 'new share name'
-//     ]);
-    
-//     $response->assertSessionHasErrors('share', 'You do not have permission to update this share.');
-    
-//     $this->assertDatabaseHas('shares', [
-//         'id' => $share->id,
-//         'amount' => $share->amount->getMinorAmount()->toInt(),
-//     ]);
-// });
