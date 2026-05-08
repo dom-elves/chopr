962f474ee87fb372b7bf08248a81f8702cea7788
Dom Elves <dom_elves@hotmail.co.uk>
2026-05-08 13:21:32 +0100

    further discrepancy improvements so values of £0.00 are not displayed

--- resources/js/Components/Debts/Debt.vue
+++ resources/js/Components/Debts/Debt.vue
@@ -39,12 +39,13 @@ const debtCurrency = computed(() => {
  * then edits the value of the debt, rather than adding a share.
  */
 const debtDiscrepancy = computed(() => {
-    const total = props.debt.shares.reduce((total, share) => total + Number(share.amount.amount), 0);
-    const amountString = Math.round((props.debt.amount.amount - total) * 100).toString();
-    const pounds = amountString.slice(0, -2);
-    const pence = amountString.slice(-2);
+    const total = Math.round(props.debt.shares.reduce((total, share) => total + Number(share.amount.amount), 0) * 100, 2);
+    const discrepancy = (total - Math.round((props.debt.amount.amount * 100))).toString();
+    
+    const pounds = discrepancy.slice(0, -2);
+    const pence = discrepancy.slice(-2);
 
-    return `${pounds}.${pence}`;
+    return discrepancy == 0 ? '' : `${pounds}.${pence}`;
 });
 
 /**
