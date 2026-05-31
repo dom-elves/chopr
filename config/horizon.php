ddda5d25fb0cea89c1269917595e3e42c7586d45
Dom Elves <dom_elves@hotmail.co.uk>
2026-05-31 22:51:52 +0100

    more supervisors in horizon, just playing around

--- config/horizon.php
+++ config/horizon.php
@@ -210,6 +210,32 @@ return [
             'timeout' => 60,
             'nice' => 0,
         ],
+        'local-supervisor-1' => [
+            'connection' => 'redis',
+            'queue' => ['default'],
+            'balance' => 'auto',
+            'autoScalingStrategy' => 'time',
+            'maxProcesses' => 1,
+            'maxTime' => 0,
+            'maxJobs' => 0,
+            'memory' => 128,
+            'tries' => 1,
+            'timeout' => 60,
+            'nice' => 0,
+        ],
+        'local-supervisor-2' => [
+            'connection' => 'redis',
+            'queue' => ['default'],
+            'balance' => 'auto',
+            'autoScalingStrategy' => 'time',
+            'maxProcesses' => 1,
+            'maxTime' => 0,
+            'maxJobs' => 0,
+            'memory' => 128,
+            'tries' => 1,
+            'timeout' => 60,
+            'nice' => 0,
+        ],
     ],
 
     'environments' => [
@@ -222,8 +248,11 @@ return [
         ],
 
         'local' => [
-            'supervisor-1' => [
-                'maxProcesses' => 3,
+            'local-supervisor-1' => [
+                'maxProcesses' => 10,
+            ],
+            'local-supervisor-2' => [
+                'maxProcesses' => 10,
             ],
         ],
     ],
